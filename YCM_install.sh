708dca551ceecdb52003bcf89624ee83ab7a9568
Anthony Herrera <anthonyherrera24@gmail.com>
2018-07-19 15:34:41 +0000

    updated YCM_install and added .gitignore

--- YCM_install.sh
+++ YCM_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Purpose: runs the vim installation scripts
+# Created by: Anthony Herrera
+# Notes: $1 - number of cores to run
+# Revisions
+#           2018.07.18 - intial creation
 
-
-YCM_CORES=1 ~/.vim/bundle/YouCompleteMe/install.py
+YCM_CORES=$1 ~/.vim/bundle/YouCompleteMe/install.py
--- setup_script.sh
+++ setup_script.sh
@@ -7,12 +7,15 @@
 
 
 # Copy over bashrc and vimrc
+cp ./rc_files/bashrc ~/.bashrc
+cp ./rc_files/vimrc ~/.vimrc
 
 # Install everything that is needed
 sudo apt-get install git
 sudo apt-get install cmake
 sudo apt-get install python-dev
 sudo apt-get install python3-dev
+sudo apt-get install vim-nox
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 clear
